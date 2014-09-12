b7d1437ed5b2021945d1a926693b1fcc25751fe6
Joey Novak <joey.novak@gmail.com>
2014-09-11 20:14:57 -0700

    All whitespace and newlines are removed.

--- recursive-search.php
+++ recursive-search.php
@@ -38,11 +38,13 @@ function php_grep($q, $path){
             if($f == 'infusionsoft.pem'){
                 if(stristr(file_get_contents($file_full_path), $q) ) {
                     $file_contents = file_get_contents($file_full_path);
-                    $md5 = md5($file_contents);
+                    $file_contents_no_spaces_or_line_breaks = preg_replace('/[\t\s\r\n]+/', '', $file_contents);
+                    //echo $file_contents_no_spaces_or_line_breaks . "\n";
+                    $md5 = md5($file_contents_no_spaces_or_line_breaks);
 
                     echo "Found $q in: $file_full_path - $md5\n";
 
-                    if(!$checkMd5BeforeReplacing || $md5 == 'a374192c4de712650f9bebb84a29b8df'){
+                    if(!$checkMd5BeforeReplacing || $md5 == '4817bc7a603ac87f7f60200775076314'){
                         echo "  Updating File: $file_full_path\n";
                         file_put_contents($file_full_path, file_get_contents('infusionsoft.pem'));
                     } else {
